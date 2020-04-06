93c2da53b20e31034734ebd04917ade0832d4b2d
vas59 <vasily.yuryev.a1@gmail.com>
2020-04-06 23:05:03 +0700

    launch patches

--- patch_clever.sh
+++ patch_clever.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Clever for PC
 patch < ../../../../patches/CMakeLists.patch
+
+cp -f ../../../../patches/files_for_clover/aruco.launch ./launch/
+cp -f ../../../../patches/files_for_clover/sitl.launch ./launch/
