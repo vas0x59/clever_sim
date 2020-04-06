1741a19eced3bd56c8025796af4284d668c293a9
vas59 <vasily.yuryev.a1@gmail.com>
2020-04-06 22:12:52 +0700

    Patcher

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+### CLEVER
+mkdir -p catkin_ws/src
+cd catkin_ws/src
+cd ../
+catkin_make
+source devel/setup.bash
+cd src
+git clone https://github.com/CopterExpress/clever.git
+
+# Patching
+
+cd clever
+cd clover 
+patch < CMakeLists.patch
+# END
+
+# DEP
+cd ../../../
+rosdep install -y --from-paths src --ignore-src -r
+
+# END
+
+catkin_make
+source devel/setup.bash
+
+### END
+
+cd ../
+
+
