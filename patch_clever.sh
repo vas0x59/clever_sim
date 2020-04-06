5f7c41e988d85a47c5818fcea8615f53af14d4ec
vas59 <vasily.yuryev.a1@gmail.com>
2020-04-06 23:08:24 +0700

    correct

--- patch_clever.sh
+++ patch_clever.sh
@@ -2,5 +2,5 @@
 # Clever for PC
 patch < ../../../../patches/CMakeLists.patch
 
-cp -f ../../../../patches/files_for_clover/aruco.launch ./launch/
-cp -f ../../../../patches/files_for_clover/sitl.launch ./launch/
+cp -f ../../../../patches/files_for_clover/launch/aruco.launch ./launch/
+cp -f ../../../../patches/files_for_clover/launch/sitl.launch ./launch/
