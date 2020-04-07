e5297de94f26ed7ffca2fd32ecb4d3e399cd4f9a
vas59 <vasily.yuryev.a1@gmail.com>
2020-04-07 20:29:52 +0700

    add Aruco model

--- install.sh
+++ install.sh
@@ -35,6 +35,8 @@ git clone https://github.com/PX4/Firmware.git
 cd Firmware
 DONT_RUN=1 make px4_sitl_default gazebo
 cp -fRa ../models/iris ./Tools/sitl_gazebo/models
+cp -fRa ../models/aruco_map_10_10 ./Tools/sitl_gazebo/models
+cp -f ../worlds/empty.world ./Tools/sitl_gazebo/worlds
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
