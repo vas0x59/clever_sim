cf1e9b66ca092d2739b3f0a0c1e1b7c0d3dbf7f6
vas59 <vasily.yuryev.a1@gmail.com>
2020-04-07 20:13:16 +0700

    model path

--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@ source catkin_ws/devel/setup.bash
 source Firmware/Tools/setup_gazebo.bash $(pwd) $(pwd)/Firmware/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware/Tools/sitl_gazebo
-export GAZEBO_MODEL_PATH=$(pwd)/Firmware/Tools/sitl_gazebo/models:$GAZEBO_MODEL_PATH
+#export GAZEBO_MODEL_PATH=$(pwd)/Firmware/Tools/sitl_gazebo/models:$GAZEBO_MODEL_PATH
