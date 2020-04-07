01a368bd890d11fa5d434fe30db6a96eddd8b139
vas59 <vasily.yuryev.a1@gmail.com>
2020-04-07 20:17:14 +0700

    map + cam

--- patch_clever.sh
+++ patch_clever.sh
@@ -4,3 +4,4 @@ patch < ../../../../patches/CMakeLists.patch
 
 cp -f ../../../../patches/files_for_clover/launch/aruco.launch ./launch/
 cp -f ../../../../patches/files_for_clover/launch/sitl.launch ./launch/
+cp -f ../../../../patches/files_for_clover/map.txt ../aruco_pose/map/
