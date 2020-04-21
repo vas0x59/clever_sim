b6a6073adbbcf5ed8645f9ff57419943d7dfba01
Vasily Yuryev <vasily.yuryev.a1@gmail.com>
2020-04-21 14:51:55 +0700

    libgstreamer1.0-dev

--- install_dep.sh
+++ install_dep.sh
@@ -5,6 +5,7 @@ sudo apt-get install ros-melodic-gazebo-ros \
                      ros-melodic-gazebo-ros-pkgs \
                      ros-melodic-gazebo-msgs \
                      ros-melodic-geographic-msgs \
+                     libgstreamer1.0-dev \
                      geographiclib-tools
 
 sudo apt-get update && sudo apt-get -y --quiet --no-install-recommends install bzip2 ca-certificates ccache cmake cppcheck curl dirmngr doxygen file g++ gcc gdb git gnupg gosu lcov libfreetype6-dev libgtest-dev libpng-dev lsb-release make ninja-build openjdk-8-jdk openjdk-8-jre openssh-client pkg-config python-pip python-pygments python-setuptools rsync shellcheck tzdata unzip wget xsltproc zip ant gazebo7 gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libeigen3-dev libgazebo7-dev libgstreamer-plugins-base1.0-dev libimage-exiftool-perl libopencv-dev libxml2-utils pkg-config protobuf-compiler libgeographic-dev geographiclib-tools libignition-math2-dev
