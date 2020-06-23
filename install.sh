b8ae84b7b6cbe3d4474bd5b2d53cfb4fc3ef2b85
Vasily Yuryev <vasily.yuryev.a1@gmail.com>
2020-06-23 23:35:24 +0700

    Remove

--- install.sh
+++ install.sh
@@ -31,24 +31,24 @@ cd ../
 
 
 ### PX4
-git clone https://github.com/CopterExpres/Firmware.git
+git clone https://github.com/CopterExpress/Firmware.git
 # echo aaa $(pwd)
-cd 
-git update --init --recursive
+cd ./Firmware
+git submodule update --init --recursive
 # echo aaa $(pwd)
-cd ./Tools/inlude
+cd ./Tools/sitl_gazebo/include
 # echo aaa $(pwd)
-patch < ../../../gazebo_opicalflow_plugin.patch
+patch < ../../../patches/gazebo_opticalflow_plugin.patch
 # echo aaa $(pwd)
 cd ../../../
 DONT_RUN=1 make px4_sitl_default gazebo
-echo  $(pwd)
+
 cp -fRa ../models/iris ./Tools/sitl_gazebo/models
-cp -fRa ../models/aruco_ap_10_10 ./Tools/sitl_gazebo/models
+cp -fRa ../models/aruco_map_10_10 ./Tools/sitl_gazebo/models
 cp -f ../worlds/empty.world ./Tools/sitl_gazebo/worlds
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
-export ROS_PACKAGE_PATH=$ROS_PAKAGE_PATH:$(pwd)
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_ATH:$(pwd)/Tools/sitl_gazebo
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 export GAZEBO_MODEL_PATH=$(pwd)/models:$GAZEBO_MODEL_PATH
 cd ../
 source ./env.sh
